a8c569a557ae11f660ba0cdfdb36a70038e6702e
Murat Kumykov <mkumykov@blackducksoftware.com>
2019-04-01 10:39:18 -0400

    Uniqie non-primary key processing added

--- generator/generator.sh
+++ generator/generator.sh
@@ -4,15 +4,25 @@
 TABLENAME=$1
 COLUMNS=$(cat ${1}.tbl  | grep -v Column | grep "|*|" | awk '{print $1}')
 PKEY_LIST=$(cat ${1}.tbl  | grep PRIMARY | cut -d '(' -f 2 | cut -d ')' -f 1)
+UKEY_LIST=$(cat ${1}.tbl  | grep UNIQUE | cut -d '(' -f 2 | cut -d ')' -f 1 | sed 's/, /,/g')
 
 FIELD_LIST=$(echo ${COLUMNS} | sed 's/ /,/g')
 VALUE_LIST=$(for i in $COLUMNS ; do echo -n "new.${i}," ; done | sed 's/,$//' )
 UPDATE_SET=$(for i in $COLUMNS ; do echo -n " ${i} = new.${i}," ; done | sed 's/,$//' )
 WHERE_CLAUSE=$(for i in $PKEY_LIST ; do echo -n " ${i%,} = new.${i%,} AND" ; done | sed 's/AND$//' )
+for i in $UKEY_LIST
+do
+  for j in $(echo $i | sed 's/,/ /g') 
+  do
+    echo -n "-- ${j%,} = new.${j%,} AND" 
+  done | sed 's/AND$//' 
+  echo 
+done
 
 echo -- $TABLENAME
 echo -- $COLUMNS
 echo -- $PKEY_LIST
+echo -- $UKEY_LIST
 echo -- $WHERE_CLAUSE
 
 cat <<!
@@ -21,16 +31,26 @@ CREATE OR REPLACE FUNCTION ${TABLENAME}_update()
 \$BODY\$
 begin
      if (select EXISTS(select 1 from pdata.${TABLENAME} where ${WHERE_CLAUSE} )) then
-        --found; update, and return null to prevent insert
-        UPDATE pdata.${TABLENAME} SET
-             ${UPDATE_SET}
-        WHERE ${WHERE_CLAUSE};
-     else
-        insert into pdata.${TABLENAME} (
-          ${FIELD_LIST} )
-        VALUES (
-          ${VALUE_LIST} );
+        --found; delete the row to allow clean insert
+        DELETE FROM pdata.${TABLENAME} WHERE ${WHERE_CLAUSE};
      end if;
+!
+for i in $UKEY_LIST
+do
+  UWHERE_CLAUSE=$(for j in $(echo $i | sed 's/,/ /g') ; do echo -n " ${j} = new.${j} AND" ; done | sed 's/AND$//' )
+cat <<!
+     if (select EXISTS(select 1 from pdata.${TABLENAME} where ${UWHERE_CLAUSE} )) then
+        --found delete the row to allow clean insert
+        DELETE FROM pdata.${TABLENAME} WHERE ${UWHERE_CLAUSE};
+     end if;
+!
+done
+
+cat <<!
+     insert into pdata.${TABLENAME} (
+        ${FIELD_LIST} )
+        VALUES (
+        ${VALUE_LIST} );
      return new;
 end
 \$BODY\$
