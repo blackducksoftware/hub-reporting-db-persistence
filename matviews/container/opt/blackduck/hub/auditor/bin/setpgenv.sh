4f09ab5efd3e5c4d1ed44e88ba754ee0a3a174d6
Murat Kumykov <mkumykov@blackducksoftware.com>
2020-01-02 13:38:41 -0500

    crontab, setpgenv  and pdataupdate script placeholedr added

---
+++ matviews/container/opt/blackduck/hub/auditor/bin/setpgenv.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+
+if [ -f $RUN_SECRETS_DIR/HUB_POSTGRES_ADMIN_PASSWORD_FILE ] 
+then
+	export PGPASSWORD=$(cat $RUN_SECRETS_DIR/HUB_POSTGRES_ADMIN_PASSWORD_FILE)
+fi
+export PGHOST=${HUB_POSTGRES_HOST:-postgres}
+export PGPORT=${HUB_POSTGRES_PORT:-5432}
+export PGUSER=${HUB_POSTGRES_USER:-blackduck}
+
+
+cp /opt/blackduck/hub/auditor/security/hub-db-admin.pem /tmp/key.pem
+chmod 600 /tmp/key.pem
+
+export PGSSLKEY=/tmp/key.pem
+export PGSSLCERT=/opt/blackduck/hub/auditor/security/hub-db-admin.crt
+
+
